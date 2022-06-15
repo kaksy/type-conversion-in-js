aaf11a8439490ab7af8b8bd3b147f22ee8e908c7
unknown <nsikakatan@gmail.com>
2022-06-15 05:36:39 -0700

    practiced the type convertion for string, numbers and boolean

---
+++ script.js
@@ -0,0 +1,35 @@
+// let str = "123";
+// console.log(typeof str);
+
+// let name = 'Mfoniso'
+// console.log(name);
+
+//  str = Number(str);
+
+//  let adamName = 'Adam';
+//  console.log(adamName);
+//  console.log(name);
+
+
+// console.log(typeof str);
+// // console.log(typeof num);
+
+
+// // converting number to string and string to number
+// let str = "123"
+// alert(typeof str);
+
+// let num = Number(str);
+// alert(typeof num);
+
+// converting string to an invalid number 
+let str = 'bruh'
+console.log( str)
+
+let num = Number(str);
+console.log( num);
+
+let name = (0)
+alert (name)
+let n = Boolean(name)
+alert(n)
